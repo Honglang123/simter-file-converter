b4ef6490bec70b2c44bcae842b2caa5f7e3e6ea4
anlian <zzjb211314@163.com>
2022-04-29 16:56:33 +0800

    Starter 模块初始化

---
+++ simter-file-converter-starter/src/main/kotlin/tech/simter/file/converter/starter/App.kt
@@ -0,0 +1,14 @@
+package tech.simter.file.converter.starter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.info.ProjectInfoAutoConfiguration
+import org.springframework.boot.runApplication
+
+fun main(args: Array<String>) {
+  runApplication<App>(*args)
+}
+
+@SpringBootApplication(
+  scanBasePackages = ["tech.simter"],
+  scanBasePackageClasses = [ProjectInfoAutoConfiguration::class])
+class App
